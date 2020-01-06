ed4fe1cb76ff18a77f0f150fb85aace1699b8eee
Sanjay Sridharan <sanjay.sridharan@alumni.utexas.net>
2020-01-05 22:36:15 -0600

    Added a REST API handler

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 "use strict";
 
-let sum = 0;
-for (let value = 1; value <= 100; value++) {
-    sum += value;
+function restApiHandler() {
+    let sum = 0;
+    for (let value = 1; value <= 100; value++) {
+        sum += value;
+    }
+
+    return sum;
 }
 
-console.log(`The sum of 1 to 100 is ${sum}.`);
+console.log(`Sanjay! The sum of 1 to 100 is ${restApiHandler()}.`);
