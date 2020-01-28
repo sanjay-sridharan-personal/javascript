e12a09ba085c57d412a5e8a0794e3a68d638fe6d
Sanjay Sridharan <sanjay.sridharan@alumni.utexas.net>
2020-01-27 18:49:36 -0600

    First attempt at calculating the result on the client

--- index.js
+++ index.js
@@ -12,10 +12,28 @@ const port = 3000;
 app.listen(port, () => `Listening on port ${port}`);
 
 function restApiHandler(request, response) {
-    let sum = 0;
-    for (let value = 1; value <= 100; value++) {
-        sum += value;
-    }
+    // let sum = 0;
+    // for (let value = 1; value <= 100; value++) {
+    //     sum += value;
+    // }
 
-    return response.send(`Sanjay! Get response: The sum of 1 to 100 is ${sum}.`);
+    return response.send(
+        `
+        <!DOCTYPE html>
+        <html lang="en">
+            <canvas id="canvas" width="800" height="800"></canvas>
+            <body>
+                <script>
+                    let sum = 0;
+                    for (let value = 1; value <= 100; value++) {
+                        sum += value;
+                    }
+
+                    let pencil = document.getElementById("canvas").getContext('2d');
+                    pencil.fillText(\`Sanjay! Get response: The sum of 1 to 100 is \${sum}.\`, 0, 0);
+                </script>
+            </body>
+        </html>
+        `
+        );
 }
