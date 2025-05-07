9606deea0f853418f609dfb81cbdbe549a6a8126
Sanjay Sridharan <sanjay.sridharan@alumni.utexas.net>
2025-05-07 00:16:20 -0500

    Hmm - hydrated octokit object looks empty, so verify empty PAT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as core from "@actions/core"
 import * as github from "@actions/github"
 
 try {
-    const octokit = github.getOctokit(core.getInput("GH_PAT"));
+    const octokit = github.getOctokit("");
     console.log(`Sanjay! octokit = ${JSON.stringify(octokit)}`);
 } catch (error) {
     core.setFailed(error.message);
