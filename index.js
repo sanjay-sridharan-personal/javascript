b20c0e9a1eefc8897502c3483e069a292ce3bcde
Sanjay Sridharan <sanjay.sridharan@alumni.utexas.net>
2025-05-07 16:39:45 -0500

    Let's try getting the token from env vars

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import * as core from "@actions/core"
-import * as github from "@actions/github"
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import process from "node:process";
 
 try {
-    const octokit = github.getOctokit(core.getInput("GH_PAT"));
+    const octokit = github.getOctokit(process.env.GH_TOKEN);
     console.log(`Sanjay! octokit = ${JSON.stringify(octokit)}`);
 } catch (error) {
     core.setFailed(error.message);
