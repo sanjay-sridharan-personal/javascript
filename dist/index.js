c4ad582406ff5bdf64873cbecdd60a2781fca6fa
GitHub Action <action@github.com>
2025-05-07 21:54:45 +0000

    action dist 📦

--- dist/index.js
+++ dist/index.js
@@ -31820,35 +31820,6 @@ module.exports = parseParams
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__nccwpck_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__nccwpck_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__nccwpck_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__nccwpck_require__.o(definition, key) && !__nccwpck_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__nccwpck_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
@@ -31865,14 +31836,18 @@ var core = __nccwpck_require__(5589);
 var github = __nccwpck_require__(1065);
 ;// CONCATENATED MODULE: external "node:process"
 const external_node_process_namespaceObject = require("node:process");
-var external_node_process_default = /*#__PURE__*/__nccwpck_require__.n(external_node_process_namespaceObject);
 ;// CONCATENATED MODULE: ./index.js
 
 
 
 
 try {
-    const octokit = github.getOctokit((external_node_process_default()).env.GH_TOKEN);
+    if (external_node_process_namespaceObject.env.GH_TOKEN) {
+        console.log(`Sanjay! env.GH_TOKEN is truthy: ${external_node_process_namespaceObject.env.GH_TOKEN[0]}`);
+    } else {
+        console.log(`Sanjay! env.GH_TOKEN is falsy: ${external_node_process_namespaceObject.env.GH_TOKEN}`);
+    }
+    const octokit = github.getOctokit(process.env.GH_TOKEN);
     console.log(`Sanjay! octokit = ${JSON.stringify(octokit)}`);
 } catch (error) {
     core.setFailed(error.message);
