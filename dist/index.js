a125f12b55f1e7d2633736e14360d5c2bcd9ef75
GitHub Action <action@github.com>
2025-05-07 22:07:26 +0000

    action dist 📦

--- dist/index.js
+++ dist/index.js
@@ -31847,7 +31847,7 @@ try {
     } else {
         console.log(`Sanjay! env.GH_TOKEN is falsy: ${external_node_process_namespaceObject.env.GH_TOKEN}`);
     }
-    const octokit = github.getOctokit(process.env.GH_TOKEN);
+    const octokit = github.getOctokit(external_node_process_namespaceObject.env.GH_TOKEN);
     console.log(`Sanjay! octokit = ${JSON.stringify(octokit)}`);
 } catch (error) {
     core.setFailed(error.message);
