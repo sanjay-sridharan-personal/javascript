9cc4916df177adf91744be1483aa6dbacd245318
Sanjay Sridharan <sanjay.sridharan@alumni.utexas.net>
2025-05-10 17:46:26 -0500

    Use the context to supply previously hardcoded values

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import { env } from "node:process";
 
 try {
     const octokit = github.getOctokit(env.GH_TOKEN);
-    const context = github.context;
-    console.log(`context = ${JSON.stringify(context)}`);
+    const payload = github.context.payload;
+    console.log(`payload = ${JSON.stringify(payload)}`);
     const { data: pullRequest } = await octokit.rest.pulls.get({
-        owner: 'sanjay-sridharan-personal',
-        repo: 'happy-birthday',
-        pull_number: 22
+        owner: `${payload.repository.owner.login}`,
+        repo: `${payload.repository.name}`,
+        pull_number: `${payload.number}`
     });
     console.log(`pullRequest = ${JSON.stringify(pullRequest)}`);
 } catch (error) {
