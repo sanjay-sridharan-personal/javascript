4c319a68e847904269dc151d70e3d441c6eb69ee
Sanjay Sridharan <sanjay.sridharan@alumni.utexas.net>
2025-05-10 18:24:45 -0500

    Add "Needs Review" label to PR

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ import { env } from "node:process";
 try {
     const octokit = github.getOctokit(env.GH_TOKEN);
     const payload = github.context.payload;
-    console.log(`payload = ${JSON.stringify(payload)}`);
-    const { data: pullRequest } = await octokit.rest.pulls.get({
+    await octokit.rest.issues.addLabels({
         owner: `${payload.repository.owner.login}`,
         repo: `${payload.repository.name}`,
-        pull_number: `${payload.number}`
+        issue_number: `${payload.number}`,
+        labels: [ "Needs Review" ]
     });
-    console.log(`pullRequest = ${JSON.stringify(pullRequest)}`);
 } catch (error) {
     core.setFailed(error.message);
 }
