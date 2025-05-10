c551e1d3c744baf52897fda9fe3891e8a1c99b18
Sanjay Sridharan <43077373+sanjay-sridharan-personal@users.noreply.github.com>
2025-05-10 09:16:51 -0500

    Test out authenticated octokit

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ import { env } from "node:process";
 
 try {
     const octokit = github.getOctokit(env.GH_TOKEN);
-    const context = github.context;
-    console.log(`Sanjay! octokit = ${JSON.stringify(octokit)}`);
+    // const context = github.context;
+    // console.log(`Sanjay! octokit = ${JSON.stringify(octokit)}`);
+    const { data: pullRequest } = await octokit.rest.pulls.get({
+        owner: 'sanjay-sridharan-personal',
+        repo: 'happy-birthday',
+        pull_number: 22,
+        mediaType: {
+          format: 'diff'
+        }
+    });
+
+    console.log(pullRequest);
 } catch (error) {
     core.setFailed(error.message);
 }
