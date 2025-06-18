0fa110a945b62455de4ae4504ac5787336865e59
Sanjay Sridharan <sanjay.sridharan@alumni.utexas.net>
2025-06-18 17:34:48 -0500

    Added before and after debugs

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 try {
+    core.debug(`Before: authenticate using ${github.token ? 'defined' : 'undefined'} token`);
     const octokit = github.getOctokit(github.token);
+    core.debug(`After: ${octokit}`);
     const payload = github.context.payload;
     await octokit.rest.issues.addLabels({
         owner: `${payload.repository.owner.login}`,
         repo: `${payload.repository.name}`,
         issue_number: `${payload.number}`,
-        labels: [ "Needs Review" ]
+        labels: [ 'Needs Review' ]
     });
 } catch (error) {
     core.setFailed(error.message);
