322d1b4788ad17967ce6111a0108fcc2b31a8fe3
Sanjay Sridharan <sanjay.sridharan@alumni.utexas.net>
2025-06-19 10:45:00 -0500

    Output PR title

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 try {
-    // const octokit = github.getOctokit(core.getInput('gh_token'));
-    const payload = github.context.payload;
-    core.info(`payload = ${JSON.stringify(payload)}`)
+    const octokit = github.getOctokit(core.getInput('gh_token'));
+    const prTitle = github.context.payload.pull_request.title;
+    core.info(`payload = ${JSON.stringify(prTitle)}`);
 } catch (error) {
     core.setFailed(error.message);
 }
