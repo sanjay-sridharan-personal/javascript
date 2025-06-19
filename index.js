de8ce2020e0bdbaf401fc394a314eac2926dea70
Sanjay Sridharan <sanjay.sridharan@alumni.utexas.net>
2025-06-19 11:02:50 -0500

    Log payload again

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 try {
-    const octokit = github.getOctokit(core.getInput('gh_token'));
+    const payload = github.context.payload;
+    core.info(`payload = ${JSON.stringify(payload)}`);
     const prTitle = github.context.payload.pull_request.title;
-    core.info(`payload = ${JSON.stringify(prTitle)}`);
+    core.info(`prTitle = ${JSON.stringify(prTitle)}`);
+    if (!prTitle.startWith('Jira-')) {
+        const octokit = github.getOctokit(core.getInput('gh_token'));
+        octokit.rest.pulls.createReviewComment({
+        });
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
